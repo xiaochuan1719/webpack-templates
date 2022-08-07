c9b2404b057ab9cb06616fce4c7d59b6d7b991c0
xiaochuan1719 <xiaochuan1719@outlook.com>
2022-08-07 20:04:50 +0800

    create template: react-app

---
+++ react-app/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+};
---
+++ react-app/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="container">
+      <h2>Hello React</h2>
+    </main>
+  );
+};
+
+export default App;
